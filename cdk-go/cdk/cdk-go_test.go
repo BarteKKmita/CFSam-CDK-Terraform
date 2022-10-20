df81cfb0a30d48aa32fb37395108724edcd68189
Bartlomiej Kmita <bartlomiej.kmita@assaabloy.com>
2022-10-20 21:20:02 +0200

    add cdk

---
+++ cdk-go/cdk/cdk-go_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+	"github.com/aws/jsii-runtime-go"
+)
+
+func TestCdkGoStack(t *testing.T) {
+	// GIVEN
+	app := awscdk.NewApp(nil)
+	// WHEN
+	stack := NewCdkGoStack(app, "CdkGoStack", nil)
+	template := assertions.Template_FromStack(stack, nil)
+	// THEN
+	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+		"VisibilityTimeout": 300,
+	})
+	template.HasResourceProperties(jsii.String("AWS::Events::Rule"), map[string]interface{}{
+		"ScheduleExpression": "cron(0/15 * ? * * *)",
+	})
+
+}
